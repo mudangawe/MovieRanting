f75a0ba46fdbd9d3190e054665955279e18b2d38
Ndivhuwo Ramashia <ndivhuwo.ng@gmail.com>
2020-03-02 15:15:06 +0200

    first commit

---
+++ Movie Ranking/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Movie_Ranking.MvcApplication" Language="C#" %>
---
+++ Movie Ranking/ViewModel/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Movie_Ranking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movie_Ranking.ViewModel
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
